273090142aba64b125182ce909eb45654254b7a8
joanaz <rz258@cornell.edu>
2015-09-19 18:08:27 -0400

    c

--- browser/js/petition/petition.js
+++ browser/js/petition/petition.js
@@ -6,6 +6,11 @@ app.config(function($stateProvider) {
       console.log($stateParams.id)
       $scope.petition = PetitionFactory.getOnePetition($stateParams.id)
       $scope.rep = RepFactory.getOneRep($scope.petition.rep)
+      $scope.socialPage = {
+        Url: 'http://loc.co/petition/' + $stateParams.id,
+        Name: $scope.petition.title,
+        ImageUrl: $scope.petition.image
+      }
     }
   });
 });
